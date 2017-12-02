a69521e4312fde7726b1de88c53f8dcaf3804c39
Saul Orea <soreaort@gmail.com>
2017-12-02 10:23:08 -0600

    Add Javadoc in the class
    Signed-off-by: Saul Orea <soreaort@gmail.com>

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,7 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
+  /*
+   *@param name of the person
+   *@return gret the person
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
