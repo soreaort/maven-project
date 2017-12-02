f0ac9e81a746128f9a244e1f29362f5c7c8ad16b
Saul Orea <soreaort@gmail.com>
2017-12-02 10:26:20 -0600

    Add an errors as test
    Signed-off-by: Saul Orea <soreaort@gmail.com>

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
    *@param name of the person
    *@return gret the person
    */
-  public final String greet(final String someone) {
+  ublic final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
